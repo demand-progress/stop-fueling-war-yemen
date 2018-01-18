398e12686ace99124050e7f735506270f32175aa
Mateo Balcorta <misterrattail@gmail.com>
2018-01-18 13:59:10 -0800

    fix page jump issue.

--- js/containers/App.jsx
+++ js/containers/App.jsx
@@ -3,6 +3,10 @@ import Footer from './Footer.jsx';
 import Main from './Main.jsx'
 
 class App extends Component {
+  componentDidMount () {
+    window.scrollTo(0, 0)
+  }
+
   render() {
       return (
         <div>
