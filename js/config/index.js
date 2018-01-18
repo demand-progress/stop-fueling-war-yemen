416382f11f6f05b2a4e1c877c9cf593a3a23fa1e
Mateo Balcorta <misterrattail@gmail.com>
2018-01-17 16:10:09 -0800

    Add actionKit config values.

--- js/config/index.js
+++ js/config/index.js
@@ -1,6 +1,6 @@
 const CONF = {
-    actionKitPageShortName: 'stop-fcc',
-    actionKitPageId: 2160
+    actionKitPageShortName: 'stop-fueling-war-yemen',
+    actionKitPageId: 2172
 };
 const URLS = {
     actionKit: 'https://act.demandprogress.org/act/',
