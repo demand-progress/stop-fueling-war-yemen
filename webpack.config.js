026d41e5118cda68505e143e1e51ce04a1b55d6c
Mateo Balcorta <misterrattail@gmail.com>
2018-01-29 11:01:06 -0800

    Revert filed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,12 @@
-const path = require('path');
-const BUILD_DIR = path.resolve('./public');
-const APP_DIR = path.resolve('./src');
-
 module.exports = [{
     devServer: {
         inline:true,
         port: 8008
     },
-    entry: APP_DIR + "/call.jsx",
+    entry: "./js/call.jsx",
     output: {
-        path: BUILD_DIR,
-        filename: "js/bundle.js"
+        path: __dirname + '/js',
+        filename: "bundle.js"
     },
     module: {
         loaders: [{
