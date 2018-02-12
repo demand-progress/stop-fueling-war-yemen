57c81d196fc1baf4fa74fe0b35c56c05b3893a84
Mateo Balcorta <misterrattail@gmail.com>
2018-02-12 13:13:38 -0800

    Updated phone script.

--- js/containers/PhoneScriptText.jsx
+++ js/containers/PhoneScriptText.jsx
@@ -3,11 +3,8 @@ import React from 'react'
 const PhoneScriptText = (props) => {
     return (
         <div>        
-            <p><strong>Here's what you can say:</strong></p>
-            <p>Hello,</p>
-            <p>I am a constituent of Senator _______________ and I am calling to ask them to support the War Powers Resolution to stop U.S. military support for the Saudi-led coalition in Yemen.</p>
-            <p>Over eight million people are facing the threat of famine as the result of my tax dollars supporting the Saudi-led coalition in Yemen’s civil war. The Sanders-Lee bipartisan, privileged resolution invoking the War Powers Resolution, requires a vote on ending U.S. military support for this war. As your constituent, I urge you to cosponsor and vote YES on the Sanders-Lee resolution to ensure that our nation stops causing suffering in a country already devastated by conflict, cholera, and famine.</p>
-            <p>Thank you.</p>
+            <p><strong>We will connect you the offices of your senators.  You can tell them:</strong></p>
+            <p>I am a constituent of Senator _____.  Please support the Sanders-Lee resolution that would end American involvement in the war on Yemen.  American military aid is fueling a humanitarian crisis, and it is time for this to stop.</p>
         </div>
     )
 }
