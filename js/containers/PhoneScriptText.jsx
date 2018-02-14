9eb4179a2fcc6c22ba2e63ebf7bc2635ca054b1c
Mateo Balcorta <misterrattail@gmail.com>
2018-02-14 11:17:44 -0800

    Add quotation marks around quote.

--- js/containers/PhoneScriptText.jsx
+++ js/containers/PhoneScriptText.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PhoneScriptText = (props) => {
     return (
         <div>       
-            <p>I am a constituent of Senator _____. Please support the Sanders-Lee resolution that would end American involvement in the war on Yemen. American military aid is fueling a humanitarian crisis, and it is time for this to stop.</p>
+            <p>"I am a constituent of Senator _____. Please support the Sanders-Lee resolution that would end American involvement in the war on Yemen. American military aid is fueling a humanitarian crisis, and it is time for this to stop."</p>
         </div>
     )
 }
