25d6a02e0a8c962b1f0a632ca4dca4c06a4aa990
Mateo Balcorta <misterrattail@gmail.com>
2018-02-28 14:25:13 -0800

    Remove header for video component.

--- js/containers/Main.jsx
+++ js/containers/Main.jsx
@@ -15,7 +15,6 @@ class Main extends Component {
                 </div>
                 
                 <div className="unit">
-                    <h4 style={{color: 'white', textAlign: 'center'}}>Mark Ruffalo explains the crisis in Yemen:</h4>
                     <Video />
                     <h4 style={{color: 'white', textAlign: 'center'}}>More information:</h4>
                     <p style={{ textAlign: 'center' }}>
