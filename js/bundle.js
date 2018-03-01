98b74b1792a377f38242f526ad70a7c64b1585b9
Mateo Balcorta <misterrattail@gmail.com>
2018-02-28 20:47:16 -0800

    Bundle.

--- js/bundle.js
+++ js/bundle.js
@@ -6104,7 +6104,43 @@
 	            { style: { color: 'white' } },
 	            'Mark Ruffalo explains the crisis in Yemen:'
 	        ),
-	        _react2.default.createElement('iframe', { width: '560', height: '315', src: 'https://www.youtube.com/embed/WaQewDoipQQ', frameBorder: '0', allow: 'autoplay; encrypted-media', allowFullScreen: true })
+	        _react2.default.createElement('iframe', { width: '560', height: '315', src: 'https://www.youtube.com/embed/WaQewDoipQQ', frameBorder: '0', allow: 'autoplay; encrypted-media', allowFullScreen: true }),
+	        _react2.default.createElement(
+	            'div',
+	            { id: 'share-button-container' },
+	            _react2.default.createElement(
+	                'div',
+	                { className: 'social-media' },
+	                _react2.default.createElement(
+	                    'div',
+	                    { className: 'button-share' },
+	                    _react2.default.createElement(
+	                        'a',
+	                        { className: 'twitter', href: 'https://twitter.com/intent/tweet?text=Our%20public%20dollars%20are%20fueling%20the%20worst%20humanitarian%20crisis%20on%20the%20globe.%20It%27s%20up%20to%20us%20to%20stop%20it.%20Urge%20your%20Sen%20to%20cosponsor%20%23SandersLeeYemen%20resolution%20%26%20cut%20off%20U.S.%20support%20for%20Saudi%20Arabia%27s%20brutal%20war%20in%20%23Yemen.', target: '_blank' },
+	                        _react2.default.createElement('img', { src: 'images/twitter_white.svg' }),
+	                        _react2.default.createElement(
+	                            'span',
+	                            null,
+	                            'Share on twitter'
+	                        )
+	                    )
+	                ),
+	                _react2.default.createElement(
+	                    'div',
+	                    { className: 'button-share' },
+	                    _react2.default.createElement(
+	                        'a',
+	                        { className: 'facebook', href: 'https://www.facebook.com/sharer.php?u=https://stopthewar.us/', target: '_blank' },
+	                        _react2.default.createElement('img', { src: 'images/facebook_white.svg' }),
+	                        _react2.default.createElement(
+	                            'span',
+	                            null,
+	                            'Share on facebook'
+	                        )
+	                    )
+	                )
+	            )
+	        )
 	    );
 	};
 	
