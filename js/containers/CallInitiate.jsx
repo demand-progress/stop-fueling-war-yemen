b334b98d694475b623245f4d2836d07544560992
Mateo Balcorta <misterrattail@gmail.com>
2018-03-08 12:56:14 -0800

    Add conditional for header text.

--- js/containers/CallInitiate.jsx
+++ js/containers/CallInitiate.jsx
@@ -59,9 +59,18 @@ class CallInitiate extends Component {
           <img src="images/phone.svg" />
         </button>)
       }
+
+      let headerText = null
+
+      if(this.props.callActionEmailForm){
+        headerText = (<h3>Now, could you make a call to your senators?</h3>)
+      } else {
+        headerText = (<h3>Thanks for signing <br/>Now, could you make a call to your senators?</h3>)
+      }
+
         return (
           <div className="bftn-form call-action-form" id="signThePetition">
-            <h3>Thanks for signing. <br/>Now, could you make a call to your senators?</h3>
+            {headerText}
             <br/><br/>
             <div style={{color: 'white', lineHeight: 1.5}}>
               <strong style={{ fontSize: "25px" }}>
