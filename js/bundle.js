b54f365e812acb8339b6cbb112f5c020beb75c45
Mateo Balcorta <misterrattail@gmail.com>
2018-03-13 15:32:55 -0700

    Bundle.

--- js/bundle.js
+++ js/bundle.js
@@ -5637,7 +5637,10 @@
 	        _react2.default.createElement(
 	            'p',
 	            null,
-	            '"I am a constituent of Senator _____. Please support the Sanders-Lee resolution S.J.Res. 54 that would end American involvement in the war in Yemen. American military aid is fueling a humanitarian crisis, and it is time for this to stop."'
+	            '"I am a constituent of Senator _____. Please support the Sanders-Lee resolution S.J.Res. 54 that would end American involvement in the war in Yemen. American military aid is fueling a humanitarian crisis, and it is time for this to stop.',
+	            _react2.default.createElement('br', null),
+	            _react2.default.createElement('br', null),
+	            'I urge the Senator to vote YES on the Sanders-Lee resolution S.J.Res. 54 this week, and NO on the Young-Shaheen resolution S.J.Res. 55, which includes vague language that could backfire and make it easier for the United States to provide unconstitutional military aid to the Saudi-led coalition in Yemen."'
 	        )
 	    );
 	};
